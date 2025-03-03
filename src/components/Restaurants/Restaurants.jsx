05b943d1a4cd491478a9abb8563bb48776e3d76b
Hiddensti <alrahmanihadi@gmail.com>
2025-03-03 22:38:35 +0300

    fetching restaurants

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -1,22 +1,53 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 
 const Restaurants = () => {
-  const { id } = useParams();
+  const [restaurants, setRestaurants] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  if (id) {
-    return (
-      <div>
-        <h1>Restaurant Details</h1>
-        <p>Restaurant ID: {id}</p>
-        <p>More details about the restaurant and menu will go here.</p>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const fetchRestaurants = async () => {
+      try {
+        const res = await fetch(`${BACKEND_URL}/restaurants`);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Failed to fetch restaurants");
+        }
+
+        setRestaurants(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRestaurants();
+  }, []);
+
+  if (loading) return <p>Loading restaurants...</p>;
+  if (error) return <p>Error: {error}</p>;
 
   return (
     <div>
-      <h1>All Restaurants</h1>
-      <p>List of restaurants will be displayed here.</p>
+      <h1>Available Restaurants</h1>
+      <ul>
+        {restaurants.map((restaurant) => (
+          <li key={restaurant._id}>
+            
+            <Link to={`/restaurant/${restaurant._id}`}>
+              <h3>{restaurant.name}</h3>
+            </Link>
+            <p>{restaurant.description}</p>
+            <p><strong>Location:</strong> {restaurant.location}</p>
+            <p><strong>Opening Hours:</strong> {restaurant.openingHours}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
